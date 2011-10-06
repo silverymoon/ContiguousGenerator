986c541614e44f64fe8383c026b7473c66edbaea
Britta Rogowski <silverymoon@gmail.com>
2011-10-06 15:26:12 +0200

    completed calculations for first version

--- contiguous_generator.coffee
+++ contiguous_generator.coffee
@@ -5,10 +5,15 @@ class ContiguousPatternGenerator
   calculate: (form) =>
     s_gauge = form.stitch_gauge / 4
     r_gauge = form.row_gauge / 4
-    @cast_on = Math.round((form.back_length * s_gauge) + 4)
+    @back_st = Math.round(form.back_length * s_gauge)
+    @cast_on = @back_st + 4
     @seam_length = Math.round(form.shoulder_seam * r_gauge)
+    armhole_rows = form.armhole_length * r_gauge
+    @one_sixth_arm_hole = Math.round(armhole_rows / 6)
+    @arm_hole_minus_one_inch_and_sixth = Math.round(armhole_rows - (@one_sixth_arm_hole + r_gauge))
+    @num_body_stitches = Math.round(s_gauge * form.bust_size)
+    @r_gauge_in_inches = Math.round(r_gauge)
+    @s_gauge_in_inches = Math.round(s_gauge)
 
 root = exports ? this
 root.ContiguousPatternGenerator = ContiguousPatternGenerator
-
-
--- contiguous_generator.js
+++ contiguous_generator.js
@@ -6,11 +6,18 @@
       this.calculate = __bind(this.calculate, this);      this.calculate(form);
     }
     ContiguousPatternGenerator.prototype.calculate = function(form) {
-      var r_gauge, s_gauge;
+      var armhole_rows, r_gauge, s_gauge;
       s_gauge = form.stitch_gauge / 4;
       r_gauge = form.row_gauge / 4;
-      this.cast_on = Math.round((form.back_length * s_gauge) + 4);
-      return this.seam_length = Math.round(form.shoulder_seam * r_gauge);
+      this.back_st = Math.round(form.back_length * s_gauge);
+      this.cast_on = this.back_st + 4;
+      this.seam_length = Math.round(form.shoulder_seam * r_gauge);
+      armhole_rows = form.armhole_length * r_gauge;
+      this.one_sixth_arm_hole = Math.round(armhole_rows / 6);
+      this.arm_hole_minus_one_inch_and_sixth = Math.round(armhole_rows - (this.one_sixth_arm_hole + r_gauge));
+      this.num_body_stitches = Math.round(s_gauge * form.bust_size);
+      this.r_gauge_in_inches = Math.round(r_gauge);
+      return this.s_gauge_in_inches = Math.round(s_gauge);
     };
     return ContiguousPatternGenerator;
   })();
